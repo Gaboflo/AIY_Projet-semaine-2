425e697bf5b06094a21cb2b03e033f8fc2994faf
Florian GABORIAUD <florian.gaboriaud@student-cs.fr>
2018-11-19 15:17:37 +0100

    Debut du resize

--- Code_Florian.py
+++ Code_Florian.py
@@ -1,1 +1,16 @@
-Test = 0
+from PIL import Image
+import numpy as np
+from resizeimage import resizeimage
+
+def resize(patern_path):
+    for file in patern_path :
+        with open(file, 'r+b') as f:
+
+            with Image.open(f) as image:
+                matrice_taille=misc.imread(file)
+                ratio=matrice_taille.shape[0,1]
+
+                image_resized= image +  '_resized'
+                cover = resizeimage.resize_cover(image, ratio[0],ratio[1])
+
+                #identification(cover)
